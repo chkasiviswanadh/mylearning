b74664437f34022c03828a9ab1c30ea91e100780
chkasiviswanadh <kasi01viswanadh@gmail.com>
2023-05-30 14:06:01 +0530

    adding shellscripting files

---
+++ scripts/devops_myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Hello Kasi"
+
+#echo $BASH
+
+name=kasiviswanadh
+
+echo "Hello ${name}, Please enter your age here:"
+
+read age
+
+echo "My age is ${age}"
+
+echo "${1}: How is the wheather today?"
+
+sleep 5
+
+echo "${2}: Its freezing cold today"
+
+sleep 5
+
+echo "guys enjoy the wheather"
---
+++ scripts/iftest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $1 = "yes" ]
+then
+echo "$1,Im the king of devops"
+else
+echo "congratulations, you are the kingmaker"
+fi
